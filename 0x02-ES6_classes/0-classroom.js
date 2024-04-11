4775965e42d54992396017b7f027b36dc1ed6263
bneayoub <bne.ayoub@gmail.com>
2024-04-11 04:34:18 +0000

    Adding Comments to meet style requirements

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,12 @@
+/**
+ * Represents a classroom.
+ */
 export default class ClassRoom {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
