1a430f75ee80983aaf7c50d021e9aa9001301133
bneayoub <bne.ayoub@gmail.com>
2024-04-11 05:11:38 +0000

    Add HolbertonCourse class in 0x02-ES6_classes/2-hbtn_course.js

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,71 @@
+/**
+ * Represents a Holberton Course.
+ */
+export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - course name.
+   * @param {Number} length - period of the course.
+   * @param {String[]} students - name of students in the course.
+   */
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+   * Gets the name.
+   */
+  get name() {
+    return this._name;
+  }
+
+  /**
+   * Sets the name.
+   */
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  /**
+   * Gets the length.
+   */
+  get length() {
+    return this._length;
+  }
+
+  /**
+   * Sets the length.
+   */
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  /**
+   * Gets the students names.
+   */
+  get students() {
+    return this._students;
+  }
+
+  /**
+   * Sets the students names.
+   */
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
